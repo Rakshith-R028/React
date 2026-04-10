112d96bc91954c00f989dc52385281113a6cec65
Rakshith-Ramachandra <rakshithr028.o@gmail.com>
2026-04-10 14:56:36 +0530

    Until section-5 video-33

---
+++ pizza-menu/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  const x = "js";
+  return <h1>Hello React!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
