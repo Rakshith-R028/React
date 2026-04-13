7bce89697e277f1b69db5845bb8550780e575070
Rakshith-Ramachandra <rakshithr028.o@gmail.com>
2026-04-13 23:29:56 +0530

    Until section-6 v-61

---
+++ state/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+]
+
+function App() {
+  const btnBgColorActive = { backgroundColor: "#7950f2" }
+  const [step, setStep] = useState(0)
+  const handlePrevious = () => {
+    if (step > 0) setStep(step - 1)
+  }
+
+  const handleNext = () => {
+    if (step < 2) setStep(step + 1)
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step === 0 ? "active" : ""}>1</div>
+        <div className={step === 1 ? "active" : ""}>2</div>
+        <div className={step === 2 ? "active" : ""}>3</div>
+      </div>
+      <div className="message">{messages[step]}</div>
+      <div className="buttons">
+        <button
+          onClick={handlePrevious}
+          style={btnBgColorActive}
+          disabled={step === 0}
+        >
+          Previous
+        </button>
+        <button
+          onClick={handleNext}
+          style={btnBgColorActive}
+          disabled={step === 2}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
